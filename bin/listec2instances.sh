75930aeead1f9475f92f9b14b0cf48d3f0240ea0
Luke Ashe-Browne <xactly@gmail.com>
2016-01-12 17:43:57 +0000

    updating listec2instances to use prefix env var

--- bin/listec2instances.sh
+++ bin/listec2instances.sh
@@ -6,11 +6,12 @@ if [[ $ENV == "" ]]; then
 	echo
 	exit 1
 fi
+AWSENVPREFIX=${AWSENVPREFIX:=""} #set this to your vpc environment prefix eg <company>-<Department>-<project>-
+#eg  export AWSENVPREFIX="acme-sales-widgets-"
+echo "AWSENVPREFIX = $AWSENVPREFIX"
 
-AWSENVPREFIX="" #set this to your vpc environment prefix eg itv-<product>-<department>-
-
-#AWSENVPREFIX and ENV will be used to make up a string matching what's in your ~/.aws/config  so make sure you include any sepreating characters.
+#ENVPREFIX and ENV will be used to make up a string matching what's in your ~/.aws/config  so make sure you include any sepreating characters.
 #Otherwise leave it blank so you can hit any env if you have plenty of non-prefixable environments you connect to.
 #
 
-aws --profile $ENVPREFIX$ENV ec2 describe-instances --output json --query "Reservations[*].Instances[*]" | jq  --raw-output '.[]|.[]|(if .Tags then (.Tags[]|select(.Key == "Service").Value) else empty end)+" " +.PrivateIpAddress+" " +.InstanceId+" " +.Placement.AvailabilityZone' |sort -s|column -t | grep -E --color '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b'
+aws --profile ${AWSENVPREFIX}${ENV} ec2 describe-instances --output json --query "Reservations[*].Instances[*]" | jq  --raw-output '.[]|.[]|(if .Tags then (.Tags[]|select(.Key == "Service").Value) else empty end)+" " +.PrivateIpAddress+" " +.InstanceId+" " +.Placement.AvailabilityZone' |sort -s|column -t | grep -E --color '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b'
